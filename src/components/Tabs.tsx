1590a24dd8c267609f47fc3f1f21edd6184554b7
Mustafa Bulut <80100508+mustafablutt@users.noreply.github.com>
2023-07-13 13:04:33 +0300

    The code has been optimized and the methods have been made better with usecallback.

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -11,36 +11,36 @@ import { useContext } from 'react';
 import { TabContext as GlobalTabContext } from '../context/TabContext';
 import { Outlet, useNavigate } from 'react-router-dom'; // Import global TabContext
 
-
 export default function LabTabs() {
   const { tabs, currentTab,  removeTab, changeTab } = useContext(GlobalTabContext)!; // Use global TabContext
   const navigate = useNavigate();
   return (
     <Box sx={{ width: '100%', typography: 'body1' }}>
-      
 
       <TabContext value={currentTab}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <TabList onChange={(event, newValue) => changeTab(newValue)} aria-label="lab API tabs example">
-  {tabs.map((tab, index) => (
-    <Tab 
-      key={index} 
-      label={
-        <div>
-          {tab.label}
-          <IconButton onClick={() => removeTab(tab)} size="small">
-            <CloseIcon />
-          </IconButton>
-        </div>
-      }
-      value={tab.value}
-      onClick={() => navigate(tab.value)} // Tab'a tıkladığında kullanıcıyı ilgili sayfaya yönlendirin 
-    />
-  ))}
-</TabList>
-
+          <TabList onChange={(event, newValue) => changeTab(newValue)} aria-label="lab API tabs example">
+            {tabs.map((tab) => (
+              <Tab 
+                key={tab.value}
+                label={
+                  <div>
+                    {tab.label}
+                    <IconButton onClick={(event) => {
+                        event.stopPropagation(); // Bunu ekleyin
+                        removeTab(tab);
+                    }} size="small">
+                      <CloseIcon />
+                    </IconButton>
+                  </div>
+                }
+                value={tab.value}
+                onClick={() => navigate(tab.value)}
+              />
+            ))}
+          </TabList>
         </Box>
-        <Outlet /> {/* Route içerisindeki çocukları görüntülemek için Outlet'i kullanın */}
+        <Outlet /> 
       </TabContext>
     </Box>
   );
--- src/components/provider/TabProvider.tsx
+++ src/components/provider/TabProvider.tsx
@@ -1,86 +1,54 @@
-import React, { useState, ReactNode } from 'react';
+import React, { useState, useCallback, ReactNode } from 'react';
 import { TabContext, Tab } from '../../context/TabContext';
 import { useNavigate } from 'react-router-dom';
 
-
 interface TabProviderProps {
-    children: ReactNode;
-  }
-  
-  export const TabProvider: React.FC<TabProviderProps> = ({ children }) => {
-    const [tabs, setTabs] = useState<Tab[]>([
-     
-    ]);
-    const navigate = useNavigate();
-   
-  
-    const [currentTab, setCurrentTab] = useState<string>('1');
-  
-    const addTab = (label: string, value: string) => {
-      const newTab = { label, value };
-      setTabs([...tabs, newTab]);
-      setCurrentTab(newTab.value);
-    };
-  
-    const removeTab = (tab: Tab) => {
-      console.log('Removing tab:', tab);
-    
-      const newTabs = tabs.filter(t => t.value !== tab.value);
-      console.log('New tabs:', newTabs);
-    
-      setTabs(newTabs);
-      
-      if (newTabs.length > 0) {
-        console.log('Current tab:', currentTab);
-        console.log('Tab to remove:', tab.value);
-    
-        // Check if the current tab is still in the newTabs array
-        const activeTabExists = newTabs.some((t) => t.value === currentTab);
-    
-        if (!activeTabExists) {
-          // If the current tab does not exist in the newTabs array, navigate to the previous one
-          const previousTab = newTabs[newTabs.length - 1];
-          console.log('Navigating to previous tab:', previousTab);
-      
-          setCurrentTab(()=>{
-            navigate(previousTab.value); 
-            return previousTab.value;
-          });
-        } else {
-          
+  children: ReactNode;
+}
 
-          console.log('Current tab still exists, not changing the current tab.');
-          // We do not need to navigate to the currentTab, so we remove the navigation call
-          setCurrentTab(()=>{
-            navigate(currentTab);  
-            changeTab(currentTab); 
-            return currentTab;
-          });
-         
-        }
+export const TabProvider: React.FC<TabProviderProps> = ({ children }) => {
+  const [tabs, setTabs] = useState<Tab[]>([]);
+  const navigate = useNavigate();
+  const [currentTab, setCurrentTab] = useState<string>('1');
+
+  const addTab = useCallback((label: string, value: string) => {
+    const newTab = { label, value };
+    setTabs((prevTabs) => [...prevTabs, newTab]);
+    setCurrentTab(newTab.value);
+  }, []);
+
+  const changeTab = useCallback((tab: string) => {
+    setCurrentTab(tab);
+  }, []);
+
+  const removeTab = useCallback((tab: Tab) => {
+    const newTabs = tabs.filter((t) => t.value !== tab.value);
+    setTabs(newTabs);
+
+    if (newTabs.length > 0) {
+      const activeTabExists = newTabs.some((t) => t.value === currentTab);
+      if (!activeTabExists) {
+        const previousTab = newTabs[newTabs.length - 1];
+        setCurrentTab(() => {
+          navigate(previousTab.value);
+          return previousTab.value;
+        });
       } else {
-        // There are no tabs remaining, so navigate to a default page
-        console.log('No tabs remaining. Navigating to default page.');
-    
-        navigate('/home'); // Or wherever you want to redirect when no tabs are open
+        setCurrentTab((currentTab) => {
+          navigate(currentTab);
+          changeTab(currentTab);
+          return currentTab;
+        });
       }
-    };
-    
+    } else {
+      navigate('/home');
+    }
+  }, [currentTab, navigate, tabs, changeTab]); // changeTab bağımlılık listesine eklendi.
 
 
-
-    
-    const changeTab = (tab: string) => {
-      setCurrentTab(tab);
-    };
-  
-    return (
-      <TabContext.Provider value={{ tabs, currentTab, addTab, removeTab, changeTab }}>
-        {children}
-      </TabContext.Provider>
-    );
-  };
-
-
-
- 
+  return (
+    <TabContext.Provider value={{ tabs, currentTab, addTab, removeTab, changeTab }}>
+      {children}
+    </TabContext.Provider>
+  );
+};
